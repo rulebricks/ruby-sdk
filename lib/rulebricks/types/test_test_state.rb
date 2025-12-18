004cd65a2531b446735ea56ddf90cc258cba9862
Siddharth Garimella <gsidsid@gmail.com>
2025-12-17 19:54:04 -0800

    Update test conditions schema

--- lib/rulebricks/types/test_test_state.rb
+++ lib/rulebricks/types/test_test_state.rb
@@ -6,7 +6,7 @@ module Rulebricks
     class TestTestState < Internal::Types::Model
       field :duration, -> { Integer }, optional: true, nullable: false
       field :response, -> { Internal::Types::Hash[String, Object] }, optional: true, nullable: false
-      field :conditions, -> { Internal::Types::Array[Internal::Types::Hash[String, Rulebricks::Types::TestTestStateConditionsItemValue]] }, optional: true, nullable: false
+      field :conditions, -> { Internal::Types::Array[Internal::Types::Hash[String, Object]] }, optional: true, nullable: false
       field :http_status, -> { Integer }, optional: true, nullable: false, api_name: "httpStatus"
       field :success_idxs, -> { Internal::Types::Array[Integer] }, optional: true, nullable: false, api_name: "successIdxs"
       field :evaluation_error, -> { Rulebricks::Types::TestTestStateEvaluationError }, optional: true, nullable: false, api_name: "evaluationError"
